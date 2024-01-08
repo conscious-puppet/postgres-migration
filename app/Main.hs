f08942aee5e21a0aa108642657d117e8c8272b23
Abhishek Singh <abhishek17513@gmail.com>
2024-01-08 20:33:40 +0530

    init

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,8 @@
+{-# LANGUAGE QuasiQuotes #-}
+
 module Main where
 
-import Control.Monad (foldM, unless, when)
+import Control.Monad (foldM, unless, void)
 import qualified Crypto.Hash as CH
 import Data.Aeson
 import Data.Aeson.Encode.Pretty
@@ -10,6 +12,8 @@ import Data.List (foldl', groupBy, sortBy)
 import Data.Maybe
 import qualified Data.Text as DT
 import qualified Database.PostgreSQL.Simple as PSQL
+import Database.PostgreSQL.Simple.SqlQQ (sql)
+import Database.PostgreSQL.Simple.Types (Only (Only))
 import qualified Database.PostgreSQL.Simple.Types as PSQL
 import GHC.Generics (Generic)
 import qualified System.Directory as SD
@@ -40,6 +44,7 @@ main = do
       else BS.writeFile migrationLockFilePath "[]" >> pure []
   refreshedMigrationLock <- refreshMigrationLock migrationLock
   BS.writeFile migrationLockFilePath (toStrict $ encodePretty refreshedMigrationLock)
+  runMigrations "dev" refreshedMigrationLock
   pure ()
 
 refreshMigrationLock :: [MigrationLock] -> IO [MigrationLock]
@@ -60,42 +65,51 @@ refreshMigrationLock migrationLock = do
         , y <- newMigrationLock
         , env x == env y
         ]
-  -- mapM_ verifyMigrationLocks migrationLocks
+  mapM_ verifyMigrationLocks migrationLocks
   return newMigrationLock
 
 verifyMigrationLocks :: ([MigrationLockInfo], [MigrationLockInfo]) -> IO ()
 verifyMigrationLocks (_, []) = return ()
 verifyMigrationLocks ([], _) = return ()
 verifyMigrationLocks (migrationLockInfos, newMigrationLockInfos) = do
-  let newMigartionLockInfos' = tail newMigrationLockInfos
-  let oldMigrationFile = last migrationLockInfos
+  let newMigartionLockInfos' = tail $ reverse newMigrationLockInfos
   let filesChanged =
         [ filePath y
         | x <- migrationLockInfos
         , y <- newMigartionLockInfos'
-        , majorVersion x == majorVersion y
-            && minorVersion x == minorVersion y
-            && pointVersion x == pointVersion y
-            && hash x /= hash y
+        , majorVersion x
+            == majorVersion y
+            && minorVersion x
+            == minorVersion y
+            && pointVersion x
+            == pointVersion y
+            && hash x
+            /= hash y
         ]
-  unless (null filesChanged) $ error $ "Following old migration files have changed, please revert the changes:\n\t" <> show filesChanged
-  let invalidMigrationFiles =
-        foldl'
-          ( \acc f ->
-              if f `notElem` migrationLockInfos
-                && ( majorVersion f
-                      <> minorVersion f
-                      <> pointVersion f
-                      < majorVersion oldMigrationFile
-                      <> minorVersion oldMigrationFile
-                      <> pointVersion oldMigrationFile
-                   )
-                then f : acc
-                else acc
-          )
-          []
-          newMigrationLockInfos
-  unless (null invalidMigrationFiles) $ error $ "Following new migration files have been added with old version, please update the version numbers:\n\t" <> show invalidMigrationFiles
+  unless (null filesChanged) $ error $ "Following old migration files have changed. Only latest migration files should be edited, please revert the changes:\n\t" <> show filesChanged
+  let oldMigrationLockInfos = filter (`elem` newMigrationLockInfos) migrationLockInfos
+  if null oldMigrationLockInfos
+    then return ()
+    else do
+      let oldMigrationFile = last oldMigrationLockInfos
+      let invalidMigrationFiles =
+            foldl'
+              ( \acc f ->
+                  if f
+                    `notElem` migrationLockInfos
+                    && ( majorVersion f
+                          <> minorVersion f
+                          <> pointVersion f
+                          < majorVersion oldMigrationFile
+                          <> minorVersion oldMigrationFile
+                          <> pointVersion oldMigrationFile
+                       )
+                    then f : acc
+                    else acc
+              )
+              []
+              newMigrationLockInfos
+      unless (null invalidMigrationFiles) $ error $ "Following new migration files have been added with older versions, please update the version numbers:\n\t" <> show invalidMigrationFiles
 
 createMigrationLockInfo :: FilePath -> IO [MigrationLockInfo]
 createMigrationLockInfo e = do
@@ -105,19 +119,23 @@ createMigrationLockInfo e = do
   migrationLockInfos' <-
     mapM
       ( \x -> do
-          let (_ : majorVersion : minorVersion : pointRelease : _) = DT.splitOn "_" $ DT.pack x
+          let (_ : majorVersion : minorVersion : pointVersion : _) = DT.splitOn "_" $ DT.pack x
           fileHash <- CH.hash <$> BS.readFile (e <> x) :: IO (CH.Digest CH.MD5)
-          return $ MigrationLockInfo (e <> x) majorVersion minorVersion pointRelease (DT.pack $ show fileHash)
+          return $ MigrationLockInfo (e <> x) majorVersion minorVersion pointVersion (DT.pack $ show fileHash)
       )
       migrations
   let groups =
-        filter (\g -> length g > 1) $
-          groupBy
+        filter (\g -> length g > 1)
+          $ groupBy
             ( \a b ->
-                majorVersion a == majorVersion b
-                  && minorVersion a == minorVersion b
-                  && pointVersion a == pointVersion b
-                  && filePath a /= filePath b
+                majorVersion a
+                  == majorVersion b
+                  && minorVersion a
+                  == minorVersion b
+                  && pointVersion a
+                  == pointVersion b
+                  && filePath a
+                  /= filePath b
             )
             migrationLockInfos'
   unless (null groups) $ error $ "Following migration files have same version:\n\t" <> show (map (map filePath) groups)
@@ -131,24 +149,65 @@ createMigrationLockInfo e = do
           migrationLockInfos'
   return migrationLockInfos
 
-runMigration :: IO ()
-runMigration = do
+runMigrations :: DT.Text -> [MigrationLock] -> IO ()
+runMigrations env' migrationLocks = do
   conn <- PSQL.connectPostgreSQL "host=localhost port=5432 dbname=postgres_migration user=bilbo password=baggins"
+  let createMigrationTableQuery =
+        [sql|
+              CREATE TABLE IF NOT EXISTS migration_table (
+                  id SERIAL PRIMARY KEY,
+                  env TEXT NOT NULL,
+                  major_version TEXT NOT NULL,
+                  minor_version TEXT NOT NULL,
+                  point_version TEXT NOT NULL,
+                  file_path TEXT NOT NULL,
+                  applied_at TIMESTAMP WITH TIME ZONE NOT NULL
+              );
+          |]
+  void $ PSQL.execute_ conn createMigrationTableQuery
 
-  -- run common dir
-  let commonRelativePath = "db-migration/common/"
-  common <- map (commonRelativePath <>) <$> SD.listDirectory commonRelativePath
-
-  commonMigration <- mapM BS.readFile common
-  -- mapM_ (PSQL.execute_ conn . PSQL.Query) commonMigration
-  let hashCommon = map CH.hash commonMigration
-  print (hashCommon :: [CH.Digest CH.MD5])
-
-  -- run the env dir, here env = dev
-  let devRelativePath = "db-migration/dev/"
-  dev <- map (devRelativePath <>) <$> SD.listDirectory devRelativePath
-
-  devMigration <- mapM BS.readFile dev
-  -- mapM_ (PSQL.execute_ conn . PSQL.Query) devMigration
-  let hashDev = map CH.hash devMigration
-  print (hashDev :: [CH.Digest CH.MD5])
+  let commonMigrations = filter (\m -> env m == "common") migrationLocks
+      envMigrations = filter (\m -> env m == env') migrationLocks
+  mapM_ (runMigrationsHelper conn) (commonMigrations <> envMigrations)
+ where
+  runMigrationsHelper :: PSQL.Connection -> MigrationLock -> IO ()
+  runMigrationsHelper conn migration = do
+    putStrLn $ "Running " <> DT.unpack (env migration) <> " migrations..."
+    let lastRanMigration =
+          [sql|
+            SELECT major_version, minor_version, point_version
+            FROM migration_table
+            WHERE
+            env = ?
+            ORDER BY
+              id DESC
+            LIMIT 1;
+          |]
+    lastRanVersion :: [(DT.Text, DT.Text, DT.Text)] <- PSQL.query conn lastRanMigration (Only (env migration) :: Only DT.Text)
+    let newScripts = case lastRanVersion of
+          [] -> migrationLockInfo migration
+          [(mjV, miV, poV)] -> filter (\c -> majorVersion c <> minorVersion c <> pointVersion c > mjV <> miV <> poV) (migrationLockInfo migration)
+          _ -> error "Unexpected error while fetching last executed migration script"
+    let updateMigrationTableQuery =
+          [sql|
+            INSERT INTO migration_table
+              (env, major_version, minor_version, point_version, file_path, applied_at)
+            VALUES
+              (?, ?, ?, ?, ?, NOW());
+          |]
+    mapM_
+      ( \script -> do
+          q <- BS.readFile (filePath script)
+          void $ PSQL.execute_ conn . PSQL.Query $ q
+          void
+            $ PSQL.execute
+              conn
+              updateMigrationTableQuery
+              ( env migration
+              , majorVersion script
+              , minorVersion script
+              , pointVersion script
+              , DT.pack $ filePath script
+              )
+      )
+      newScripts
---
+++ db-migration/dev/migration_000_000_0001_baseline.sql
@@ -0,0 +1,8 @@
+-- configurations1 table
+insert into configurations1 values (2, 'key2', 'value2', NOW(), NOW());
+
+-- configurations2 table
+insert into configurations2 values (2, 'key2', 'value2', NOW(), NOW());
+
+-- configurations3 table
+INSERT INTO configurations3 VALUES (2, 'key2', 'value2', NOW(), NOW());
